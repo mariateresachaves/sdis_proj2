9422718b8bff99f618e52b0298e41b4e9aaafbe3
Teresa Chaves <up201306842@fe.up.pt>
2017-05-09 16:04:05 +0400

    load properties file

--- src/Client.java
+++ src/Client.java
@@ -9,65 +9,81 @@ import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
+import java.util.logging.Level;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Client {
 	private static PublicKey pubKey;
 	private static PrivateKey privKey;
-	
+	private static int pubKeyCode = 101;
+	private static int messageCode = 101;
+
 	public static void main(String args[]) throws Exception {
+		if (args.length != 1) {
+			System.out.println("[?] Usage: Client <PropertiesFile>");
+			System.exit(Util.ErrorCode.ERR_WRONG_ARGS.ordinal());
+		}
+
+		// Load properties file
+		Util.loadPropertiesFile(args[0]);
+
 		// Keys
 		generateKeys();
+
+		String keyMessage = String.format("%d-%s-%s", pubKeyCode, Util.getProperties().getProperty("KEY_SIZE"), pubKey);
+
+		System.out.println(keyMessage);
 		
 		Socket sock = new Socket("localhost", 3000);
-		
+
 		// reading from keyboard (keyRead object)
 		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		// sending to client (pwrite object)
 		OutputStream ostream = sock.getOutputStream();
 		PrintWriter pwrite = new PrintWriter(ostream, true);
-		
-		 // receiving from server ( receiveRead  object)
-	     InputStream istream = sock.getInputStream();
-	     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
-	     
-	     System.out.println("Start the chitchat, type and press Enter key");
-	     
-	     // Sends Public Key
-	     System.out.println(pubKey);
-	     
-	     //byte[] publicBytes = Base64.getEncoder().encode(pubKey.toString().getBytes());
-	     pwrite.println(pubKey.toString());
-	     // flush the data
-	  	 pwrite.flush();
-	     
-	     String receiveMessage, sendMessage;               
-	     while(true) {
-	    	// keyboard reading
+
+		// receiving from server (receiveRead object)
+		InputStream istream = sock.getInputStream();
+		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+
+		System.out.println("Start the chitchat, type and press Enter key");
+
+		// Sends Public Key
+		System.out.println(pubKey);
+
+		// byte[] publicBytes =
+		// Base64.getEncoder().encode(pubKey.toString().getBytes());
+		pwrite.println(pubKey.toString());
+		// flush the data
+		pwrite.flush();
+
+		String receiveMessage, sendMessage;
+		while (true) {
+			// keyboard reading
 			sendMessage = keyRead.readLine();
-			
+
 			// sending to server
 			pwrite.println(sendMessage);
 			// flush the data
 			pwrite.flush();
-			
-			//receive from server
-			if((receiveMessage = receiveRead.readLine()) != null)
+
+			// receive from server
+			if ((receiveMessage = receiveRead.readLine()) != null)
 				// displaying at DOS prompt
 				System.out.println("Original received: " + receiveMessage);
-	     }
+		}
 	}
-	
+
 	private static void generateKeys() throws Exception {
 		Security.addProvider(new BouncyCastleProvider());
 		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA", "BC");
-		
+
 		kpg.initialize(4096);
-		
+
 		KeyPair kp = kpg.genKeyPair();
-		
+
 		pubKey = kp.getPublic();
 		privKey = kp.getPrivate();
 	}
---
+++ src/Util.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.Properties;
+
+public class Util {
+
+	/**
+	 * Error codes
+	 */
+	public enum ErrorCode {
+		ERR_WRONG_ARGS
+	}
+	
+	private static Properties p = new Properties();
+
+	public static Properties loadPropertiesFile(File f) throws FileNotFoundException, IOException {
+		p = new Properties();
+		InputStream inputStream;
+
+		inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(f.getPath());
+
+		if (inputStream != null) {
+			p.load(inputStream);
+		} else {
+			throw new FileNotFoundException("property file '" + p + "' not found in the classpath");
+		}
+
+		return p;
+	}
+
+	public static Properties loadPropertiesFile(String file) throws FileNotFoundException, IOException {
+		File f = new File(file);
+
+		return loadPropertiesFile(f);
+	}
+
+	public static Properties getProperties() {
+		return p;
+	}
+
+	public void saveProperties(File f) throws IOException {
+			OutputStream out = new FileOutputStream(f);
+			p.store(out, "Saved" + Calendar.getInstance().getTime());
+	}
+
+}
