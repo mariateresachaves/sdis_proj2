e7e85f20ac2e3717b2345ddba7fd3069d2d9cf9d
Teresa Chaves <up201306842@fe.up.pt>
2017-05-14 11:05:08 +0200

    thread to receive a message

---
+++ src/ReceiveThread.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+public class ReceiveThread extends Thread {
+	
+	private static DatagramSocket socket;
+	private static MulticastSocket mcast_socket;
+	private static InetAddress mcast_address;
+	
+	public void run() {
+
+		try {
+			socket = new DatagramSocket();
+			mcast_socket = new MulticastSocket(Integer.parseInt(Util.getProperties().getProperty("MC_PORT")));
+			mcast_address = InetAddress.getByName(Util.getProperties().getProperty("MC_IP"));
+			
+			// Join multicast channel
+			mcast_socket.joinGroup(mcast_address);
+			
+			// Receive response
+			byte[] buf = new byte[256];
+			DatagramPacket packet_received = new DatagramPacket(buf, buf.length);
+			
+			mcast_socket.receive(packet_received);
+
+			String response = new String(packet_received.getData());
+			System.out.println("msg: " + response);
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Teste
+		
+		/*while(true) {
+			try {
+				Thread.sleep(5000);
+				System.out.println("Hello from a thread!");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}*/
+    }
+}
