75fdf9771fabf152adb112de4193979d5d5b6d67
Pedro Rodrigues <up201104253@fe.up.pt>
2017-05-22 23:39:16 +0100

    troca de mensagens encriptadas a funcionar

--- src/groupServer/ClientCrontoller.java
+++ src/groupServer/ClientCrontoller.java
@@ -2,9 +2,12 @@ package groupServer;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -15,6 +18,8 @@ import java.util.Scanner;
 
 import org.bouncycastle.bcpg.*;
 import org.bouncycastle.openpgp.PGPException;
+
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 import BouncyCastle.BCPGPDecryptor;
 import BouncyCastle.BCPGPEncryptor;
@@ -39,30 +44,48 @@ public class ClientCrontoller {
 		String locationID = Configs.Util.getProperties().getProperty("HS_ID");
 
 		String req = String.format("%s:JOIN:%s", locationID, pwd);
-
+		String fileE ="";
 		try {
-			req = encryptMsg(req);
+			fileE= encryptMsg(req);
+			
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
-		try (
+		try {
 		// 4 test
-		Socket kkSocket = new Socket("ltqibw3wnfrjwmmm.onion", 80);
-				PrintWriter out = new PrintWriter(kkSocket.getOutputStream(),
-						true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(
-						kkSocket.getInputStream()));) {
+		Socket kkSocket = new Socket(serverID, 80);
+				
+		/*PrintWriter out = new PrintWriter(kkSocket.getOutputStream(),true);
+		BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));) {
 
-			out.write(req);
+			
+			out.write(b);
+
 			out.flush();
+*/
+				File file = new File(fileE);
+		        // Get the size of the file
+		        long length = file.length();
+		        byte[] bytes = new byte[16 * 1024];
+		        InputStream in = new FileInputStream(file);
+		        OutputStream out = kkSocket.getOutputStream();
 
+		        int count;
+		        while ((count = in.read(bytes)) > 0) {
+		            out.write(bytes, 0, count);
+		        }
+
+		        out.close();
+		        in.close();
+		        kkSocket.close();
 			// TODO:CHECK IF OK
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
+				
 		return false;
 	}
 
@@ -83,6 +106,7 @@ public class ClientCrontoller {
 			tempf = File.createTempFile("msgE", ".txt");
 			System.out.println(tempf.getAbsolutePath());
 			enc.encryptFile(temp, tempf);
+			return tempf.getAbsolutePath();
 
 		} catch (PGPException e1) {
 			// TODO Auto-generated catch block
@@ -91,7 +115,7 @@ public class ClientCrontoller {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+/*
 		if (tempf != null) {
 			Path path = Paths.get(tempf.getAbsolutePath());
 			byte[] data = null;
@@ -101,10 +125,9 @@ public class ClientCrontoller {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			String encrypt = new String(data);
-			return encrypt;
-		}
-		return null;
+	*/		
+			return null;
+		
 	}
 
 	public String getMessages() throws FileNotFoundException {
